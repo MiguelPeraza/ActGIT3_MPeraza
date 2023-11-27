c941b584efbd298abee7e7759d397e0a131cf7bb
Miguel <a_miguel.peraza.pena@iespablopicasso.es>
2023-11-27 09:08:44 +0100

    Añadimos el programa aleatorio

---
+++ RGIT/src/app.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+public class app {
+
+      public static void main(String[] args) {
+          mostrarNumerosAleatorios();
+      }
+
+      public static void mostrarNumerosAleatorios() {
+          
+          Random random = new Random();
+
+          System.out.println("20 números aleatorios entre 1 y 10:");
+
+          
+          for (int i = 0; i < 20; i++) {
+              int numeroAleatorio = random.nextInt(10) + 1; 
+              System.out.print(numeroAleatorio + " ");
+          }
+      }
+  }
+
