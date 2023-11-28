82b476efd9bf5f180e426f28d6a0a233122d4d9c
Miguel <a_miguel.peraza.pena@iespablopicasso.es>
2023-11-28 08:54:38 +0100

    Se actualiza el cambio del programa

--- RGIT/src/programa2.java
+++ RGIT/src/programa2.java
@@ -5,30 +5,34 @@ public class programa2 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		
-		System.out.print("Introduce el primer número entero: ");
-		int numero1 = scanner.nextInt();
+		 System.out.print("Introduce el primer número entero: ");
+	        int numero1 = scanner.nextInt();
 
-		int numero2;
-		do {
-			System.out.print("Introduce un segundo número entero distinto al primero: ");
-			numero2 = scanner.nextInt();
-			if (numero1 == numero2) {
-				System.out.println("Error: El segundo número debe ser distinto al primero.");
-			}
-		} while (numero1 == numero2);
+	        System.out.print("Introduce el segundo número entero (distinto del primero): ");
+	        int numero2 = scanner.nextInt();
 
-		
-		int menor = Math.min(numero1, numero2);
-		int mayor = Math.max(numero1, numero2);
+	        while (numero1 == numero2) {
+	            System.out.println("Los números deben ser distintos. Vuelve a introducir el segundo número.");
+	            System.out.print("Introduce el segundo número entero (distinto del primero): ");
+	            numero2 = scanner.nextInt();
+	        }
 
-		
-		System.out.println("Números enteros entre " + menor + " y " + mayor + " con incremento de 7:");
+	        System.out.print("Introduce el tercer número entero: ");
+	        int numero3 = scanner.nextInt();
 
-		for (int i = menor; i <= mayor; i += 7) {
-			System.out.println(i);
-		}
+	        System.out.println("Números entre " + numero1 + " y " + numero2 + " con incremento de 7:");
 
-		scanner.close();
+	        int menor = Math.min(numero1, numero2);
+	        int mayor = Math.max(numero1, numero2);
+
+	        for (int i = menor; i <= mayor; i += 7) {
+	            System.out.print(i + " ");
+	        }
+
+	        if (numero3 >= menor && numero3 <= mayor) {
+	            System.out.println("El tercer número (" + numero3 + ") está entre el primer y segundo número.");
+	        }
+
+	        scanner.close();
+	    }
 	}
-}
